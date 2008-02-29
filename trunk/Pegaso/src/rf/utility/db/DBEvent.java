a5d72db4828e8aedc5c21f59441e604b84b22490
rocco.fusella <rocco.fusella@6a84275c-d73c-0410-af58-0be8a4f824f5>
2008-02-29 00:34:59 +0000

    merge bookings

--- trunk/Pegaso/src/rf/utility/db/DBEvent.java
+++ trunk/Pegaso/src/rf/utility/db/DBEvent.java
@@ -1,37 +1,40 @@
+/**
+ *
+ */
 package rf.utility.db;
 
 /**
  * @author Hunter
- * 
+ *
  */
 public class DBEvent {
-	private int nCamera;
-	private String tabella;
+    private String tabella;
+    private int nCamera;
 
-	public DBEvent() {
-		this.tabella = new String();
-		this.nCamera = -1;
-	}
+    public DBEvent(String tabella, int nCamera) {
+	this.tabella = tabella;
+	this.nCamera = nCamera;
+    }
 
-	public DBEvent(String tabella, int nCamera) {
-		this.tabella = tabella;
-		this.nCamera = nCamera;
-	}
+    public DBEvent() {
+	this.tabella = new String();
+	this.nCamera = -1;
+    }
 
-	public int getNCamera() {
-		return nCamera;
-	}
+    public int getNCamera() {
+	return nCamera;
+    }
 
-	public String getTabella() {
-		return tabella;
-	}
+    public void setNCamera(int camera) {
+	nCamera = camera;
+    }
 
-	public void setNCamera(int camera) {
-		nCamera = camera;
-	}
+    public String getTabella() {
+	return tabella;
+    }
 
-	public void setTabella(String tabella) {
-		this.tabella = tabella;
-	}
+    public void setTabella(String tabella) {
+	this.tabella = tabella;
+    }
 
 }

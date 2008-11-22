044d555c81085e2f3ce63fc9e30cb85818c0c210
rocco.fusella <rocco.fusella@6a84275c-d73c-0410-af58-0be8a4f824f5>
2008-11-22 01:40:11 +0000

    generated hibernate file

--- trunk/Pegaso/hibernate/erreeffe/entity/AggiuntaHome.java
+++ trunk/Pegaso/hibernate/erreeffe/entity/AggiuntaHome.java
@@ -1,6 +1,6 @@
 package erreeffe.entity;
-// Generated 19-nov-2008 20.32.15 by Hibernate Tools 3.2.2.GA
 
+// Generated 20-nov-2008 2.05.45 by Hibernate Tools 3.2.2.GA
 
 import java.util.List;
 import javax.naming.InitialContext;
@@ -17,115 +17,108 @@ import static org.hibernate.criterion.Example.create;
  */
 public class AggiuntaHome {
 
-    private static final Log log = LogFactory.getLog(AggiuntaHome.class);
+	private static final Log log = LogFactory.getLog(AggiuntaHome.class);
 
-    private final SessionFactory sessionFactory = getSessionFactory();
-    
-    protected SessionFactory getSessionFactory() {
-        try {
-            return (SessionFactory) new InitialContext().lookup("SessionFactory");
-        }
-        catch (Exception e) {
-            log.error("Could not locate SessionFactory in JNDI", e);
-            throw new IllegalStateException("Could not locate SessionFactory in JNDI");
-        }
-    }
-    
-    public void persist(Aggiunta transientInstance) {
-        log.debug("persisting Aggiunta instance");
-        try {
-            sessionFactory.getCurrentSession().persist(transientInstance);
-            log.debug("persist successful");
-        }
-        catch (RuntimeException re) {
-            log.error("persist failed", re);
-            throw re;
-        }
-    }
-    
-    public void attachDirty(Aggiunta instance) {
-        log.debug("attaching dirty Aggiunta instance");
-        try {
-            sessionFactory.getCurrentSession().saveOrUpdate(instance);
-            log.debug("attach successful");
-        }
-        catch (RuntimeException re) {
-            log.error("attach failed", re);
-            throw re;
-        }
-    }
-    
-    public void attachClean(Aggiunta instance) {
-        log.debug("attaching clean Aggiunta instance");
-        try {
-            sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
-            log.debug("attach successful");
-        }
-        catch (RuntimeException re) {
-            log.error("attach failed", re);
-            throw re;
-        }
-    }
-    
-    public void delete(Aggiunta persistentInstance) {
-        log.debug("deleting Aggiunta instance");
-        try {
-            sessionFactory.getCurrentSession().delete(persistentInstance);
-            log.debug("delete successful");
-        }
-        catch (RuntimeException re) {
-            log.error("delete failed", re);
-            throw re;
-        }
-    }
-    
-    public Aggiunta merge(Aggiunta detachedInstance) {
-        log.debug("merging Aggiunta instance");
-        try {
-            Aggiunta result = (Aggiunta) sessionFactory.getCurrentSession()
-                    .merge(detachedInstance);
-            log.debug("merge successful");
-            return result;
-        }
-        catch (RuntimeException re) {
-            log.error("merge failed", re);
-            throw re;
-        }
-    }
-    
-    public Aggiunta findById( int id) {
-        log.debug("getting Aggiunta instance with id: " + id);
-        try {
-            Aggiunta instance = (Aggiunta) sessionFactory.getCurrentSession()
-                    .get("erreeffe.entity.Aggiunta", id);
-            if (instance==null) {
-                log.debug("get successful, no instance found");
-            }
-            else {
-                log.debug("get successful, instance found");
-            }
-            return instance;
-        }
-        catch (RuntimeException re) {
-            log.error("get failed", re);
-            throw re;
-        }
-    }
-    
-    public List<Aggiunta> findByExample(Aggiunta instance) {
-        log.debug("finding Aggiunta instance by example");
-        try {
-            List<Aggiunta> results = (List<Aggiunta>) sessionFactory.getCurrentSession()
-                    .createCriteria("erreeffe.entity.Aggiunta")
-                    .add( create(instance) )
-            .list();
-            log.debug("find by example successful, result size: " + results.size());
-            return results;
-        }
-        catch (RuntimeException re) {
-            log.error("find by example failed", re);
-            throw re;
-        }
-    } 
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext()
+					.lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException(
+					"Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Aggiunta transientInstance) {
+		log.debug("persisting Aggiunta instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Aggiunta instance) {
+		log.debug("attaching dirty Aggiunta instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Aggiunta instance) {
+		log.debug("attaching clean Aggiunta instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Aggiunta persistentInstance) {
+		log.debug("deleting Aggiunta instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public Aggiunta merge(Aggiunta detachedInstance) {
+		log.debug("merging Aggiunta instance");
+		try {
+			Aggiunta result = (Aggiunta) sessionFactory.getCurrentSession()
+					.merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Aggiunta findById(int id) {
+		log.debug("getting Aggiunta instance with id: " + id);
+		try {
+			Aggiunta instance = (Aggiunta) sessionFactory.getCurrentSession()
+					.get("erreeffe.entity.Aggiunta", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List<Aggiunta> findByExample(Aggiunta instance) {
+		log.debug("finding Aggiunta instance by example");
+		try {
+			List<Aggiunta> results = (List<Aggiunta>) sessionFactory
+					.getCurrentSession().createCriteria(
+							"erreeffe.entity.Aggiunta").add(create(instance))
+					.list();
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
 }
-
--- trunk/Pegaso/hibernate/erreeffe/entity/BancaHome.java
+++ trunk/Pegaso/hibernate/erreeffe/entity/BancaHome.java
@@ -1,6 +1,6 @@
 package erreeffe.entity;
-// Generated 19-nov-2008 20.32.15 by Hibernate Tools 3.2.2.GA
 
+// Generated 20-nov-2008 2.05.45 by Hibernate Tools 3.2.2.GA
 
 import java.util.List;
 import javax.naming.InitialContext;
@@ -17,115 +17,108 @@ import static org.hibernate.criterion.Example.create;
  */
 public class BancaHome {
 
-    private static final Log log = LogFactory.getLog(BancaHome.class);
+	private static final Log log = LogFactory.getLog(BancaHome.class);
 
-    private final SessionFactory sessionFactory = getSessionFactory();
-    
-    protected SessionFactory getSessionFactory() {
-        try {
-            return (SessionFactory) new InitialContext().lookup("SessionFactory");
-        }
-        catch (Exception e) {
-            log.error("Could not locate SessionFactory in JNDI", e);
-            throw new IllegalStateException("Could not locate SessionFactory in JNDI");
-        }
-    }
-    
-    public void persist(Banca transientInstance) {
-        log.debug("persisting Banca instance");
-        try {
-            sessionFactory.getCurrentSession().persist(transientInstance);
-            log.debug("persist successful");
-        }
-        catch (RuntimeException re) {
-            log.error("persist failed", re);
-            throw re;
-        }
-    }
-    
-    public void attachDirty(Banca instance) {
-        log.debug("attaching dirty Banca instance");
-        try {
-            sessionFactory.getCurrentSession().saveOrUpdate(instance);
-            log.debug("attach successful");
-        }
-        catch (RuntimeException re) {
-            log.error("attach failed", re);
-            throw re;
-        }
-    }
-    
-    public void attachClean(Banca instance) {
-        log.debug("attaching clean Banca instance");
-        try {
-            sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
-            log.debug("attach successful");
-        }
-        catch (RuntimeException re) {
-            log.error("attach failed", re);
-            throw re;
-        }
-    }
-    
-    public void delete(Banca persistentInstance) {
-        log.debug("deleting Banca instance");
-        try {
-            sessionFactory.getCurrentSession().delete(persistentInstance);
-            log.debug("delete successful");
-        }
-        catch (RuntimeException re) {
-            log.error("delete failed", re);
-            throw re;
-        }
-    }
-    
-    public Banca merge(Banca detachedInstance) {
-        log.debug("merging Banca instance");
-        try {
-            Banca result = (Banca) sessionFactory.getCurrentSession()
-                    .merge(detachedInstance);
-            log.debug("merge successful");
-            return result;
-        }
-        catch (RuntimeException re) {
-            log.error("merge failed", re);
-            throw re;
-        }
-    }
-    
-    public Banca findById( long id) {
-        log.debug("getting Banca instance with id: " + id);
-        try {
-            Banca instance = (Banca) sessionFactory.getCurrentSession()
-                    .get("erreeffe.entity.Banca", id);
-            if (instance==null) {
-                log.debug("get successful, no instance found");
-            }
-            else {
-                log.debug("get successful, instance found");
-            }
-            return instance;
-        }
-        catch (RuntimeException re) {
-            log.error("get failed", re);
-            throw re;
-        }
-    }
-    
-    public List<Banca> findByExample(Banca instance) {
-        log.debug("finding Banca instance by example");
-        try {
-            List<Banca> results = (List<Banca>) sessionFactory.getCurrentSession()
-                    .createCriteria("erreeffe.entity.Banca")
-                    .add( create(instance) )
-            .list();
-            log.debug("find by example successful, result size: " + results.size());
-            return results;
-        }
-        catch (RuntimeException re) {
-            log.error("find by example failed", re);
-            throw re;
-        }
-    } 
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext()
+					.lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException(
+					"Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Banca transientInstance) {
+		log.debug("persisting Banca instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Banca instance) {
+		log.debug("attaching dirty Banca instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Banca instance) {
+		log.debug("attaching clean Banca instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Banca persistentInstance) {
+		log.debug("deleting Banca instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public Banca merge(Banca detachedInstance) {
+		log.debug("merging Banca instance");
+		try {
+			Banca result = (Banca) sessionFactory.getCurrentSession().merge(
+					detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Banca findById(long id) {
+		log.debug("getting Banca instance with id: " + id);
+		try {
+			Banca instance = (Banca) sessionFactory.getCurrentSession().get(
+					"erreeffe.entity.Banca", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List<Banca> findByExample(Banca instance) {
+		log.debug("finding Banca instance by example");
+		try {
+			List<Banca> results = (List<Banca>) sessionFactory
+					.getCurrentSession()
+					.createCriteria("erreeffe.entity.Banca").add(
+							create(instance)).list();
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
 }
-

1451399911cb8348ecb0f346ba35d9fa42120f89
rocco.fusella <rocco.fusella@6a84275c-d73c-0410-af58-0be8a4f824f5>
2009-07-20 23:43:08 +0000

    aggiunto mapping...da testare

---
+++ src/it/infolabs/hibernate/RepartiHome.java
@@ -0,0 +1,124 @@
+package it.infolabs.hibernate;
+
+// Generated 21-lug-2009 1.40.00 by Hibernate Tools 3.2.4.GA
+
+import java.util.List;
+import javax.naming.InitialContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import static org.hibernate.criterion.Example.create;
+
+/**
+ * Home object for domain model class Reparti.
+ * @see it.infolabs.hibernate.Reparti
+ * @author Hibernate Tools
+ */
+public class RepartiHome {
+
+	private static final Log log = LogFactory.getLog(RepartiHome.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext()
+					.lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException(
+					"Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Reparti transientInstance) {
+		log.debug("persisting Reparti instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Reparti instance) {
+		log.debug("attaching dirty Reparti instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Reparti instance) {
+		log.debug("attaching clean Reparti instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Reparti persistentInstance) {
+		log.debug("deleting Reparti instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public Reparti merge(Reparti detachedInstance) {
+		log.debug("merging Reparti instance");
+		try {
+			Reparti result = (Reparti) sessionFactory.getCurrentSession()
+					.merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Reparti findById(long id) {
+		log.debug("getting Reparti instance with id: " + id);
+		try {
+			Reparti instance = (Reparti) sessionFactory.getCurrentSession()
+					.get("it.infolabs.hibernate.Reparti", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List<Reparti> findByExample(Reparti instance) {
+		log.debug("finding Reparti instance by example");
+		try {
+			List<Reparti> results = (List<Reparti>) sessionFactory
+					.getCurrentSession().createCriteria(
+							"it.infolabs.hibernate.Reparti").add(
+							create(instance)).list();
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}

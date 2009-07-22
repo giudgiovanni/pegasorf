fcbb98d574a36babb3e8ddf244ffdb050d0a2e10
rocco.fusella <rocco.fusella@6a84275c-d73c-0410-af58-0be8a4f824f5>
2009-07-22 00:21:43 +0000

    mapping

---
+++ mapping/it/infolabs/hibernate/ArticoliHome.java
@@ -0,0 +1,124 @@
+package it.infolabs.hibernate;
+
+// Generated 21-lug-2009 1.40.00 by Hibernate Tools 3.2.4.GA
+
+import java.util.List;
+import javax.naming.InitialContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import static org.hibernate.criterion.Example.create;
+
+/**
+ * Home object for domain model class Articoli.
+ * @see it.infolabs.hibernate.Articoli
+ * @author Hibernate Tools
+ */
+public class ArticoliHome {
+
+	private static final Log log = LogFactory.getLog(ArticoliHome.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext()
+					.lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException(
+					"Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Articoli transientInstance) {
+		log.debug("persisting Articoli instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Articoli instance) {
+		log.debug("attaching dirty Articoli instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Articoli instance) {
+		log.debug("attaching clean Articoli instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Articoli persistentInstance) {
+		log.debug("deleting Articoli instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public Articoli merge(Articoli detachedInstance) {
+		log.debug("merging Articoli instance");
+		try {
+			Articoli result = (Articoli) sessionFactory.getCurrentSession()
+					.merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Articoli findById(long id) {
+		log.debug("getting Articoli instance with id: " + id);
+		try {
+			Articoli instance = (Articoli) sessionFactory.getCurrentSession()
+					.get("it.infolabs.hibernate.Articoli", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List<Articoli> findByExample(Articoli instance) {
+		log.debug("finding Articoli instance by example");
+		try {
+			List<Articoli> results = (List<Articoli>) sessionFactory
+					.getCurrentSession().createCriteria(
+							"it.infolabs.hibernate.Articoli").add(
+							create(instance)).list();
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
---
+++ mapping/it/infolabs/hibernate/EnteHome.java
@@ -0,0 +1,124 @@
+package it.infolabs.hibernate;
+
+// Generated 21-lug-2009 1.40.00 by Hibernate Tools 3.2.4.GA
+
+import java.util.List;
+import javax.naming.InitialContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import static org.hibernate.criterion.Example.create;
+
+/**
+ * Home object for domain model class Ente.
+ * @see it.infolabs.hibernate.Ente
+ * @author Hibernate Tools
+ */
+public class EnteHome {
+
+	private static final Log log = LogFactory.getLog(EnteHome.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext()
+					.lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException(
+					"Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Ente transientInstance) {
+		log.debug("persisting Ente instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Ente instance) {
+		log.debug("attaching dirty Ente instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Ente instance) {
+		log.debug("attaching clean Ente instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Ente persistentInstance) {
+		log.debug("deleting Ente instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public Ente merge(Ente detachedInstance) {
+		log.debug("merging Ente instance");
+		try {
+			Ente result = (Ente) sessionFactory.getCurrentSession().merge(
+					detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Ente findById(long id) {
+		log.debug("getting Ente instance with id: " + id);
+		try {
+			Ente instance = (Ente) sessionFactory.getCurrentSession().get(
+					"it.infolabs.hibernate.Ente", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List<Ente> findByExample(Ente instance) {
+		log.debug("finding Ente instance by example");
+		try {
+			List<Ente> results = (List<Ente>) sessionFactory
+					.getCurrentSession().createCriteria(
+							"it.infolabs.hibernate.Ente").add(create(instance))
+					.list();
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
